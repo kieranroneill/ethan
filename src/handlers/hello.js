eee00bb0710118ed3077a0bb7bb34e4243759f95
Kieran O'Neill <kieranroneill@googlemail.com>
2017-04-26 13:34:16 +0100

    Fix linting isssues

--- src/handlers/hello.js
+++ src/handlers/hello.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 
 export default function hello(event, context, callback) {
-    console.log(_.snakeCase('My furst lambda function'));
+    //console.log(_.snakeCase('My furst lambda function'));
 
     callback(null, _.snakeCase('fuck yeah'));
 }
